4030f4bbca537508dc8386a8076dc60acbad608a
Stian Grepperud <stian.grepperud@puzzlepart.com>
2023-11-20 12:59:30 +0100

    Removed console.log

--- SPFx/src/webparts/deviationForm/components/DeviationForm/DeviationForm.tsx
+++ SPFx/src/webparts/deviationForm/components/DeviationForm/DeviationForm.tsx
@@ -78,8 +78,6 @@ const DeviationForm = ({ form, setSelectedForm, breadcrumbState, toFormSelection
 
     const isFirstRender = useRef(true);
 
-    console.log(state.values);
-
     useEffect(() => {
         const types = fieldTypes;
         setState(prevState => {
