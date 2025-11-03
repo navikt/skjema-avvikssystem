8ef91bd774a4b532081b7c9de06b649ea8888298
Stian Grepperud <stian.grepperud@puzzlepart.com>
2025-11-03 09:35:35 +0100

    Update error messages and remove empty string values before submit

--- SPFx/src/config/ActionsHandler.ts
+++ SPFx/src/config/ActionsHandler.ts
@@ -42,6 +42,7 @@ export default class ActionsHandler {
         for (const key in values) {
             if (Object.prototype.hasOwnProperty.call(values, key)) {
                 if (values.form === 'Physical security') values.stateOrMunicipalityService = 'Unsure';
+                if (values[key] === '') delete values[key];
                 if (!includes(fieldsToInclude, key) || key === 'personalInfoLost') {
                     delete values[key];
                 } else if (key === 'category' && values[key] === 'Violation of privacy requirements') {
