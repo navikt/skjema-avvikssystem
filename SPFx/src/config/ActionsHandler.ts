02b83c398dab7fbd22c1b5c43d7a43bcf87e7195
Stian Grepperud <stian.grepperud@puzzlepart.com>
2025-11-14 09:26:57 +0100

    Don't pass the value for selected municipality if it's registered with org number 'x'

--- SPFx/src/config/ActionsHandler.ts
+++ SPFx/src/config/ActionsHandler.ts
@@ -43,6 +43,7 @@ export default class ActionsHandler {
             if (Object.prototype.hasOwnProperty.call(values, key)) {
                 if (values.form === 'Physical security') values.stateOrMunicipalityService = 'Unsure';
                 if (values[key] === '') delete values[key];
+                if (key === 'selectedMunicipality' && values[key] === 'x') delete values[key];
                 if (!includes(fieldsToInclude, key) || key === 'personalInfoLost') {
                     delete values[key];
                 } else if (key === 'category' && values[key] === 'Violation of privacy requirements') {
